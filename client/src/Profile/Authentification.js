174f478d1843fd34c7a816805526d4d353a24146
Temayoo <Matt.yo95280@gmail.com>
2024-03-12 16:32:31 +0100

    Debut Login

--- client/src/Profile/Authentification.js
+++ client/src/Profile/Authentification.js
@@ -1,5 +1,5 @@
+// Authentification.js
 import React, { useState } from 'react';
-
 
 const Authentification = () => {
   const [username, setUsername] = useState('');
@@ -7,24 +7,39 @@ const Authentification = () => {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-
-
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <label>
-        username:
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        password:
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </label>
-      <br />
-      <button type="submit">Se connecter</button>
-    </form>
+    <div className="flex items-center justify-center h-screen">
+      <form className="max-w-md mx-auto m-8 p-6 bg-white rounded shadow-md">
+        <label className="block mb-4">
+          <span className="text-gray-700">Username:</span>
+          <input
+            type="text"
+            className="mt-1 p-2 border rounded w-full"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+
+        <label className="block mb-4">
+          <span className="text-gray-700">Password:</span>
+          <input
+            type="password"
+            className="mt-1 p-2 border rounded w-full"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Se connecter
+        </button>
+      </form>
+    </div>
   );
 };
 
