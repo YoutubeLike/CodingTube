383052782f519cfea19e55bb5e9a150a32bfa3e1
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2024-03-13 08:57:45 +0100

    test short page

---
+++ client/src/Shorts/short.js
@@ -0,0 +1,17 @@
+import "./short.css";
+import React from "react";
+
+function Short() {
+  return (
+    <div id="largeThing">
+      <div id="videoRectangle">
+        <p>Blaze</p>
+        <p>Titre qualitatif</p>
+      </div>
+
+      <div>p2</div>
+    </div>
+  );
+}
+
+export default Short;
