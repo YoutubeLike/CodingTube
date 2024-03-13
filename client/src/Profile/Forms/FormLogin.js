721c41a13e18508471019e2568f243639b342492
Temayoo <Matt.yo95280@gmail.com>
2024-03-13 16:49:00 +0100

    Rectification de Padding entre les inputs

--- client/src/Profile/Forms/FormLogin.js
+++ client/src/Profile/Forms/FormLogin.js
@@ -7,28 +7,30 @@ class FormLogin extends React.Component {
   render() {
     return (
       <>
-        <label>
-          <input
-            className='block mb-2 text-sm font-medium text-gray-900 dark:text-black w-full px-4 py-4 rounded-none bg-gray-200'
-            type="text"
-            name="usernameOrMail"
-            placeholder="Email or Username"
-            required
-            value={this.props.LoginData.usernameOrMail}
-            onChange={this.props.onLoginChange}
-          />
-        </label>
-        <label>
-          <input
-            className='block mb-2 text-sm font-medium text-gray-900 dark:text-black w-full px-4 py-4 rounded-none bg-gray-200'
-            type="password"
-            name="password"
-            placeholder="Password"
-            required
-            value={this.props.LoginData.password}
-            onChange={this.props.onLoginChange}
-          />
-        </label>
+        <div className='flex flex-col space-y-4'>
+          <label>
+            <input
+              className='block mb-2 text-sm font-medium text-gray-900 dark:text-black w-full px-4 py-4 rounded-none bg-gray-200'
+              type="text"
+              name="usernameOrMail"
+              placeholder="Email or Username"
+              required
+              value={this.props.LoginData.usernameOrMail}
+              onChange={this.props.onLoginChange}
+            />
+          </label>
+          <label>
+            <input
+              className='block mb-2 text-sm font-medium text-gray-900 dark:text-black w-full px-4 py-4 rounded-none bg-gray-200'
+              type="password"
+              name="password"
+              placeholder="Password"
+              required
+              value={this.props.LoginData.password}
+              onChange={this.props.onLoginChange}
+            />
+          </label>
+        </div>
       </>
     );
   }
