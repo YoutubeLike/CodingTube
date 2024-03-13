3bee1f3eae6f9c0ec2da1402135eb0aa756b0366
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2024-03-13 11:09:41 +0100

    fixed conflict

--- client/src/Shorts/short.js
+++ client/src/Shorts/short.js
@@ -5,7 +5,6 @@ function Short() {
   return (
     <div id="largeThing">
       <div id="videoRectangle">
-<<<<<<< HEAD
         <div>
           <p>Blaze</p>
           <p>Titre qualitatif</p>
@@ -18,14 +17,7 @@ function Short() {
         <span className="m-3 flex sm:h-5 sm:w-5 lg:h-10 lg:w-10 items-center justify-center rounded-full bg-gray-light"><img className='scale-50' src="images/comment.png"/></span>
         <span className="m-3 flex sm:h-5 sm:w-5 lg:h-10 lg:w-10 items-center justify-center rounded-full bg-gray-light"><img className='scale-50' src="images/share.png"/></span>
         <span className="m-3 flex sm:h-5 sm:w-5 lg:h-10 lg:w-10 items-center justify-center rounded-full bg-gray-light"><img className='scale-50'src="images/3dots.png"/></span>
-      </div>
-=======
-        <p>Blaze</p>
-        <p>Titre qualitatif</p>
-      </div>
-
-      <div>p2</div>
->>>>>>> ae57d1765a438e580ed3b05f7b152feae1624684
+      </div>7d1765a438e580ed3b05f7b152feae1624684
     </div>
   );
 }
