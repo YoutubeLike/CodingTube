57ac30469eda30466456ceb4355055880452cf69
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2024-03-13 11:00:22 +0100

    right bar

---
+++ client/src/Shorts/short.js
@@ -0,0 +1,25 @@
+import "./short.css";
+import React from "react";
+
+function Short() {
+  return (
+    <div id="largeThing">
+      <div id="videoRectangle">
+        <div>
+          <p>Blaze</p>
+          <p>Titre qualitatif</p>
+        </div>
+      </div>
+
+      <div id="rightBar">
+        <span className="m-3 flex sm:h-5 sm:w-5 lg:h-10 lg:w-10 items-center justify-center rounded-full bg-gray-light"><img className='scale-50' src="images/like.png"/></span>
+        <span className="m-3 flex sm:h-5 sm:w-5 lg:h-10 lg:w-10 items-center justify-center rounded-full bg-gray-light"><img className='scale-50' src="images/dislike.png"/></span>
+        <span className="m-3 flex sm:h-5 sm:w-5 lg:h-10 lg:w-10 items-center justify-center rounded-full bg-gray-light"><img className='scale-50' src="images/comment.png"/></span>
+        <span className="m-3 flex sm:h-5 sm:w-5 lg:h-10 lg:w-10 items-center justify-center rounded-full bg-gray-light"><img className='scale-50' src="images/share.png"/></span>
+        <span className="m-3 flex sm:h-5 sm:w-5 lg:h-10 lg:w-10 items-center justify-center rounded-full bg-gray-light"><img className='scale-50'src="images/3dots.png"/></span>
+      </div>
+    </div>
+  );
+}
+
+export default Short;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,8 +4,33 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1024',
+      xl: '1280px',
+    },
 
+    colors: {
+      'gray-dark': '#404040',
+      'gray': '#a1a1a1',
+      'gray-light': '#e3e4e6',
+    },
+    
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  }
+}
