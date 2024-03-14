23085b96a7955110ee0a8d763066cab1ea1a473e
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2024-03-14 11:23:29 +0100

    fixed docker not connecting bdd

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,7 @@ import "./index.css";
 import Autre, {loader as appLoader} from './Autre'
 
 import App from "./App" 
+import Short from "./short/index"
 
 const router = createBrowserRouter([
   {
@@ -30,6 +31,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <Short />
   </React.StrictMode>
 );
--- client/src/short/index.js
+++ client/src/short/index.js
@@ -3,21 +3,42 @@ import React from "react";
 
 function Short() {
   return (
-    <div id="all">
+    <div className="flex justify-center">
       <div id="videoContainer">
-        <video id="short" src="videos/1.mp4" width="1080" height="1920" muted autoPlay loop/>
-        <div className='flex flex-col justify-between h-full w-full group'>
-
-          <div className='flex justify-between items-start gradient opacity-0 group-hover:opacity-100 transition ease-in-out'>
-              <button className="h-[5vh] w-[5vh]"><img src="images/whitePlayButton.png"/></button>
-              <button className="h-[5vh] w-[5vh]"><img src="images/whiteSoundButton.png"/></button>
+        <video
+          id="short"
+          src="videos/1.mp4"
+          width="1080"
+          height="1920"
+          muted
+          autoPlay
+          loop
+        />
+        <div className="flex flex-col justify-between h-full w-full group">
+          <div className="flex justify-between items-start gradient opacity-0 group-hover:opacity-100 transition ease-in-out">
+            <button className="h-[5vh] w-[5vh]">
+              <img src="images/whitePlayButton.png" />
+            </button>
+            <button className="h-[5vh] w-[5vh]">
+              <img src="images/whiteSoundButton.png" />
+            </button>
           </div>
 
           <div id="metadataContainer" className="mb-[2vh] items-end">
             <div className="mb-[1vh] flex items-center">
-              <button className="h-[5.7vh] w-[5.7vh] rounded-full overflow-hidden"><img src="picture.png" alt="photo" className="h-[5.7vh] w-[5.7vh]"/></button>
-              <button className="ml-[0.95vh] font-semibold text-[2.5vh]">@ZachChoi</button>
-              <button className="ml-[0.95vh] px-[1.5vh] py-[0.95vh] bg-white text-black rounded-full text-[2vh]">Subscribe</button>
+              <button className="h-[5.7vh] w-[5.7vh] rounded-full overflow-hidden">
+                <img
+                  src="picture.png"
+                  alt="photo"
+                  className="h-[5.7vh] w-[5.7vh]"
+                />
+              </button>
+              <button className="ml-[0.95vh] font-semibold text-[2.5vh]">
+                @ZachChoi
+              </button>
+              <button className="ml-[0.95vh] px-[1.5vh] py-[0.95vh] bg-white text-black rounded-full text-[2vh]">
+                Subscribe
+              </button>
             </div>
             <p className="text-[2.5vh]">Would you eat this? #shorts</p>
           </div>
@@ -26,23 +47,35 @@ function Short() {
 
       <div className="flex flex-col justify-end">
         <div className="m-[0.95vh] flex flex-col items-center">
-          <button className="h-[5.7vh] w-[5.7vh] flex items-center justify-center rounded-full bg-gray-light hover:bg-gray"><img className='scale-50' src="images/like.png"/></button>
+          <button className="h-[5.7vh] w-[5.7vh] flex items-center justify-center rounded-full bg-gray-light hover:bg-gray">
+            <img className="scale-50" src="images/like.png" />
+          </button>
           <p className="text-[1.9vh]">342124</p>
         </div>
         <div className="m-[0.95vh] flex flex-col items-center">
-          <button className="h-[5.7vh] w-[5.7vh] flex items-center justify-center rounded-full bg-gray-light hover:bg-gray"><img className='scale-50' src="images/dislike.png"/></button>
+          <button className="h-[5.7vh] w-[5.7vh] flex items-center justify-center rounded-full bg-gray-light hover:bg-gray">
+            <img className="scale-50" src="images/dislike.png" />
+          </button>
           <p className="text-[1.9vh]">Dislike</p>
         </div>
         <div className="m-[0.95vh] flex flex-col items-center">
-          <button className="h-[5.7vh] w-[5.7vh] flex items-center justify-center rounded-full bg-gray-light hover:bg-gray"><img className='scale-50' src="images/comment.png"/></button>
+          <button className="h-[5.7vh] w-[5.7vh] flex items-center justify-center rounded-full bg-gray-light hover:bg-gray">
+            <img className="scale-50" src="images/comment.png" />
+          </button>
           <p className="text-[1.9vh]">1432</p>
         </div>
         <div className="m-[0.95vh] flex flex-col items-center">
-          <button className="h-[5.7vh] w-[5.7vh] flex items-center justify-center rounded-full bg-gray-light hover:bg-gray"><img className='scale-50' src="images/share.png"/></button>
+          <button className="h-[5.7vh] w-[5.7vh] flex items-center justify-center rounded-full bg-gray-light hover:bg-gray">
+            <img className="scale-50" src="images/share.png" />
+          </button>
           <p className="text-[1.9vh]">Share</p>
         </div>
-        <button className="m-[0.95vh] h-[5.7vh] w-[5.7vh] flex items-center justify-center rounded-full bg-gray-light hover:bg-gray"><img className='scale-50' src="images/3dots.png"/></button>
-        <button className="m-[0.95vh] h-[5.7vh] w-[5.7vh] flex items-center justify-center rounded bg-gray-light" > </button>
+        <button className="m-[0.95vh] h-[5.7vh] w-[5.7vh] flex items-center justify-center rounded-full bg-gray-light hover:bg-gray">
+          <img className="scale-50" src="images/3dots.png" />
+        </button>
+        <button className="m-[0.95vh] h-[5.7vh] w-[5.7vh] flex items-center justify-center rounded bg-gray-light">
+          {" "}
+        </button>
       </div>
     </div>
   );
