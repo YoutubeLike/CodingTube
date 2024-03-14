7a5bf2abb697a7d1d7f016a077723ce32e14f7ef
Lina <lina.meniai@edu.esiee-it.fr>
2024-03-14 10:49:58 +0100

    backup - shorts (front fixes: play & volume button)

--- client/src/Shorts/short.js
+++ client/src/Shorts/short.js
@@ -15,24 +15,34 @@ function Short() {
 
           <div id="metadataContainer" className="mb-[2vh] items-end">
             <div className="mb-[1vh] flex items-center">
-              <button className="rounded-full overflow-hidden"><img src="picture.png" alt="photo" className="h-4 w-4 sm:h-7 sm:w-7 md:h-10 md:w-10 lg:h-13 lg:w-13 xl:h-16 xl:w-16"/></button>
-              <button className="ml-3 font-semibold text-3xl">@ZachChoi</button>
-              <button className="ml-3 px-5 py-3 bg-white text-black rounded-full text-2xl">Subscribe</button>
+              <button className="h-[5.7vh] w-[5.7vh] rounded-full overflow-hidden"><img src="picture.png" alt="photo" className="h-[5.7vh] w-[5.7vh]"/></button>
+              <button className="ml-[0.95vh] font-semibold text-[2.5vh]">@ZachChoi</button>
+              <button className="ml-[0.95vh] px-[1.5vh] py-[0.95vh] bg-white text-black rounded-full text-[2vh]">Subscribe</button>
             </div>
-            <p className="text-2xl">Would you eat this? #shorts</p>
+            <p className="text-[2.5vh]">Would you eat this? #shorts</p>
           </div>
-
         </div>
-
       </div>
 
       <div className="flex flex-col justify-end">
-        <button className="m-2 flex h-[5vh] w-[5vh] items-center justify-center rounded-full bg-gray-light hover:bg-gray"><img className='scale-50' src="images/like.png"/></button>
-        <button className="m-3 flex h-[5vh] w-[5vh] items-center justify-center rounded-full bg-gray-light hover:bg-gray"><img className='scale-50' src="images/dislike.png"/></button>
-        <button className="m-3 flex h-[5vh] w-[5vh] items-center justify-center rounded-full bg-gray-light hover:bg-gray"><img className='scale-50' src="images/comment.png"/></button>
-        <button className="m-3 flex h-[5vh] w-[5vh] items-center justify-center rounded-full bg-gray-light hover:bg-gray"><img className='scale-50' src="images/share.png"/></button>
-        <button className="m-3 flex h-[5vh] w-[5vh] items-center justify-center rounded-full bg-gray-light hover:bg-gray"><img className='scale-50'src="images/3dots.png"/></button>
-        <button className="m-3 flex h-[5vh] w-[5vh]6 items-center justify-center rounded bg-gray-light" > </button>
+        <div className="m-[0.95vh] flex flex-col items-center">
+          <button className="h-[5.7vh] w-[5.7vh] flex items-center justify-center rounded-full bg-gray-light hover:bg-gray"><img className='scale-50' src="images/like.png"/></button>
+          <p className="text-[1.9vh]">342124</p>
+        </div>
+        <div className="m-[0.95vh] flex flex-col items-center">
+          <button className="h-[5.7vh] w-[5.7vh] flex items-center justify-center rounded-full bg-gray-light hover:bg-gray"><img className='scale-50' src="images/dislike.png"/></button>
+          <p className="text-[1.9vh]">Dislike</p>
+        </div>
+        <div className="m-[0.95vh] flex flex-col items-center">
+          <button className="h-[5.7vh] w-[5.7vh] flex items-center justify-center rounded-full bg-gray-light hover:bg-gray"><img className='scale-50' src="images/comment.png"/></button>
+          <p className="text-[1.9vh]">1432</p>
+        </div>
+        <div className="m-[0.95vh] flex flex-col items-center">
+          <button className="h-[5.7vh] w-[5.7vh] flex items-center justify-center rounded-full bg-gray-light hover:bg-gray"><img className='scale-50' src="images/share.png"/></button>
+          <p className="text-[1.9vh]">Share</p>
+        </div>
+        <button className="m-[0.95vh] h-[5.7vh] w-[5.7vh] flex items-center justify-center rounded-full bg-gray-light hover:bg-gray"><img className='scale-50' src="images/3dots.png"/></button>
+        <button className="m-[0.95vh] h-[5.7vh] w-[5.7vh] flex items-center justify-center rounded bg-gray-light" > </button>
       </div>
     </div>
   );
