c621ff04dd2ef32b6861c16e8fdc5cac36d4a74d
Kwak <kilian.dumaire@gmail.com>
2024-03-14 13:57:53 +0100

    return to main + header file

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import React, { useEffect, useState } from 'react';
 import Autre from "./Autre"
 import { Outlet, Route, Routes} from "react-router-dom"
 import "./index.css"
+import Header from './search/header';
 
 function App() {
   return (
     <>
       <div>
         <Outlet />
-        <p className="text-3xl font-bold underline"> HEADER </p>
+        <Header />
       </div>
     </>
   );
