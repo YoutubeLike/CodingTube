6c2b0cba2fa92b7c56c3df4275225495d3175400
Elwan Beauchot <elwan.beauchot-le-bon@edu.esiee-it.fr>
2024-03-18 16:31:35 +0100

    FIx on register form
    when the form is empty, an error appears

--- server/src/profil/router.js
+++ server/src/profil/router.js
@@ -13,49 +13,73 @@ const {
 router.post("/register", async (req, res) => {
   const registerData = req.body.registerData; // Extracting registration data from request body
   try {
-    // Check if the username contains '@'
-    if (registerData.username.includes("@")) {
-      console.log("Username cannot contain '@'");
-      return res.status(400).json({ error: "Username cannot contain '@'" });
-    }
-
-    // Regular expression to validate email address format
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(registerData.mail)) {
-      console.log("Invalid email address");
-      return res.status(400).json({ error: "Invalid email address" });
-    }
-
-    // Check if username and email already exist
-    const usernameExist = registerData.username ? await CheckIfUsernameExist(registerData.username) : false;
-    const mailExist = registerData.mail ? await CheckIfMailExist(registerData.mail) : false;
-
-    // Check if passwords match
-    const passwordMatch = await CheckIfPasswordMatch(registerData.password, registerData.confirmPassword);
-
-    if (usernameExist) {
-      console.log("Username is already taken");
-      return res.status(400).json({ error: "Username is already taken" });
-    } else if (mailExist) {
-      console.log("Mail is already taken");
-      return res.status(400).json({ error: "Mail is already taken" });
-    } else if (registerData.password && registerData.confirmPassword && registerData.username && registerData.mail) {
-      // Validation of password using regular expression
-      const passwordRegex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).{8,}$/;
-      if (!passwordRegex.test(registerData.password)) {
-        console.log("Password does not meet requirements");
-        return res.status(400).json({ error: "Password must contain at least 8 characters, 1 uppercase, 1 special character, and 1 digit" });
+    if (
+      registerData.username != "" ||
+      registerData.mail != "" ||
+      registerData.password != "" ||
+      registerData.confirmPassword != ""
+    ) {
+      // Check if the username contains '@'
+      if (registerData.username.includes("@")) {
+        console.log("Username cannot contain '@'");
+        return res.status(400).json({ error: "Username cannot contain '@'" });
       }
-      if (registerData.password == registerData.confirmPassword) {
-        await InsertUser(registerData);
-        console.log("User inserted successfully");
-        return res.status(200).json({ message: "User registered successfully" });
-      } else {
-        return res.status(400).json({ error: "Passwords do not match" });
+
+      // Regular expression to validate email address format
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(registerData.mail)) {
+        console.log("Invalid email address");
+        return res.status(400).json({ error: "Invalid email address" });
       }
+
+      // Check if username and email already exist
+      const usernameExist = registerData.username
+        ? await CheckIfUsernameExist(registerData.username)
+        : false;
+      const mailExist = registerData.mail
+        ? await CheckIfMailExist(registerData.mail)
+        : false;
+
+      // Check if passwords match
+      const passwordMatch = await CheckIfPasswordMatch(
+        registerData.password,
+        registerData.confirmPassword
+      );
+
+      if (usernameExist) {
+        console.log("Username is already taken");
+        return res.status(400).json({ error: "Username is already taken" });
+      } else if (mailExist) {
+        console.log("Mail is already taken");
+        return res.status(400).json({ error: "Mail is already taken" });
+      } else if (
+        registerData.password &&
+        registerData.confirmPassword &&
+        registerData.username &&
+        registerData.mail
+      ) {
+        // Validation of password using regular expression
+        const passwordRegex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).{8,}$/;
+        if (!passwordRegex.test(registerData.password)) {
+          console.log("Password does not meet requirements");
+          return res.status(400).json({
+            error:
+              "Password must contain at least 8 characters, 1 uppercase, 1 special character, and 1 digit",
+          });
+        }
+        if (registerData.password == registerData.confirmPassword) {
+          await InsertUser(registerData);
+          console.log("User inserted successfully");
+          return res
+            .status(200)
+            .json({ message: "User registered successfully" });
+        } else {
+          return res.status(400).json({ error: "Passwords do not match" });
+        }
+      } 
     } else {
-      return res.status(400).json({ error: "Fields can't be empty" });
-    }
+        return res.status(400).json({ error: "Fields can't be empty" });
+      }
   } catch (error) {
     console.error("Error during user registration:", error);
     return res.sendStatus(500);
@@ -72,10 +96,15 @@ router.post("/login", async (req, res) => {
     const mailExist = await CheckIfMailExist(loginData.usernameOrMail);
 
     // Get password associated with username or email from the database
-    const passwordFromDb = await GetPasswordFromUsernameOrEmail(loginData.usernameOrMail);
+    const passwordFromDb = await GetPasswordFromUsernameOrEmail(
+      loginData.usernameOrMail
+    );
 
     // Check if password matches with the one in the database
-    const isPasswordMatch = await CheckIfPasswordMatch(loginData.password, passwordFromDb);
+    const isPasswordMatch = await CheckIfPasswordMatch(
+      loginData.password,
+      passwordFromDb
+    );
 
     console.log(loginData.password);
     console.log(passwordFromDb);
@@ -83,11 +112,13 @@ router.post("/login", async (req, res) => {
 
     console.log(usernameExist, mailExist);
 
-    if (loginData.usernameOrMail != "" && loginData.password != "") {
+    if (loginData.usernameOrMail != "" || loginData.password != "") {
       if (usernameExist || mailExist) {
         if (isPasswordMatch) {
           console.log("User signed in successfully");
-          return res.status(200).json({ message: "User signed in successfully" });
+          return res
+            .status(200)
+            .json({ message: "User signed in successfully" });
         } else {
           return res.status(400).json({ error: "Incorrect password" });
         }
