986f7b5e54f065a660aa932fcf38f062d11de16b
PetitBidule <theo.saint-amand@orange.fr>
2024-03-19 13:28:32 +0100

    stripe

--- client/src/premium/YoutubePremiumView.js
+++ client/src/premium/YoutubePremiumView.js
@@ -1,9 +1,14 @@
 import "../App.css";
-import React, { useEffect, useState } from "react";
+import Reacts, { useEffect, useState } from "react";
 import HeroBanner from "./components/herobanner";
 import Section1 from "./components/section1";
 import Section2 from "./components/section2";
 import Section from "./components/section";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutPage from "./CheckoutPage";
 
 function YoutubePremium() {
   const [message, setMessage] = useState("");
@@ -19,8 +24,31 @@ function YoutubePremium() {
       <Section1 />
       <Section2 />
       <Section />
+      <App />
     </div>
   );
 }
 
 export default YoutubePremium;
+
+const stripePromise = loadStripe(
+  "pk_test_51OuE4UJis95gsMso7jUBhcYW3gvChETZUDg77xtHGrAGa8ChM8IYhzyjKPP6ofTrrasHgGHSls9JOASj2RECn7gC00vpnVS9hb"
+);
+
+function App() {
+  const options = {
+    mode: "payment",
+    amount: 1099,
+    currency: "usd",
+    // Customizable with appearance API.
+    appearance: {
+      /*...*/
+    },
+  };
+
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      <CheckoutPage />
+    </Elements>
+  );
+}
--- server/src/premium/controller.js
+++ server/src/premium/controller.js
@@ -1,10 +1,29 @@
+const stripe = require("stripe")(
+  "sk_test_51OuE4UJis95gsMso4SX5WfWiAOuQ1Uhf38cEGOQjKXNHlQyayDUc8u9gmYqZhsNdWfeDyXKq2TTSpHjPRe6rL54p00hjEthPmf"
+);
+const express = require("express");
+const app = express();
+
+app.use(express.static("."));
+
+app.post("/create-intent", async (req, res) => {
+  const intent = await stripe.paymentIntents.create({
+    amount: 1099,
+    currency: "usd",
+    automatic_payment_methods: { enabled: true },
+  });
+  res.json({ client_secret: intent.client_secret });
+});
+
+app.listen(3000, () => {
+  console.log("Running on port 3000");
+});
 const premium = (req, res) => {
   const name = req.params.user;
   res.send(name);
-
-  mariadb.pool.query("SELECT * FROM user").then((value) => {
-    res.send(value[0]["username"]);
-  });
+  // mariadb.pool.query("SELECT * FROM user").then((value) => {
+  //   res.send(value[0]["username"]);
+  // });
 };
 
 module.exports = {
