f47a6957027ccc449dcc0577725c2d0acf68f3db
Matthieu Laurent <ml.laurent.matthieu@gmail.com>
2024-03-19 17:10:06 +0100

    Timeline: Definitive logo added on left menu, CSS 80%¨done on the left menu, left menu now has display of the channel the current user is following

--- server/src/timeline/router.js
+++ server/src/timeline/router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { timelineRequest } = require('./controllerTimelineRequest.js')
 const { addViewTimelineRequest } = require('./controllerAddViewVideoTimeline.js')
+const { subscriptionListMenu } = require('./controllerSubscriptionListMenu.js');
 
 console.log("router timeline");
 
@@ -11,4 +12,8 @@ router.get('/timeline-request', timelineRequest)
 
 // Add view to video with video_id
 router.get('/addView-request/:videoId', addViewTimelineRequest)
+
+// Get the list of subscriptions
+router.get('/subscribe-list-request-menu', subscriptionListMenu)
+
 module.exports = router
