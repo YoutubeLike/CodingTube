908c82a66f1c6ddbdfe26510c7b88f8bf2cb2738
Stefan Dupont <dupontstef80@gmail.com>
2024-03-20 10:40:26 +0100

    fix(live): thumbnail didn't exist

--- server/src/live/controller.js
+++ server/src/live/controller.js
@@ -7,17 +7,19 @@ const saveThumbnail = ((req, res) =>
     const test = ffmpeg().input("http://live:8090/live/" + user + ".flv")
     test.inputOptions("-ss 00:00:01")
     test.outputOptions("-frames:v 1")
+    
     test.save("/app/back/public/" + user + ".jpg")
-    res.sendFile("/app/back/public" + user + ".jpg");
+    // res.sendFile("/app/back/public" + user + ".jpg");
 })
 
 const sendThumbnail = ((req, res) => {
-    if(fs.existsSync("/app/back/public" + req.query.user + ".jpg"))
+    console.log(fs.existsSync("/app/back/public/" + req.query.user + ".jpg"))
+    if(fs.existsSync("/app/back/public/" + req.query.user + ".jpg"))
     {
-        res.sendFile("/app/back/public" + req.query.user + ".jpg");
+        res.sendFile("/app/back/public/" + req.query.user + ".jpg");
     } else 
     {
-        res.sendFile("https://www.pawnamerica.com/images/no-photo.png");
+        res.sendFile("/app/back/src/public/default.png");
     }
 })
 
