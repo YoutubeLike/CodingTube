e5377e4e27233b7d729859956439862501b70ac9
Cralak <115760654+Cralak@users.noreply.github.com>
2024-03-20 10:00:17 +0100

    save to fix docker problem

--- client/src/short/shortRequest.js
+++ client/src/short/shortRequest.js
@@ -2,14 +2,12 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function ShortRequest() {
-  const [videosInfos, setVideosInfos] = useState([]);
   useEffect(() => {
     const fetchVideos = async () => {
       try {
         const response = await axios.get(
           "http://localhost:5000/api/short/short-request"
         );
-        setVideosInfos(response.data);
         return response.data;
       } catch (error) {
         console.error("Error fetching videos:", error);
