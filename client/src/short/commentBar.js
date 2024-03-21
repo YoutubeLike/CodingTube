8916ced9187029d122256345b1341573f3e44b05
Lina <lina.meniai@edu.esiee-it.fr>
2024-03-21 15:42:52 +0100

    shorts - comment bar progress

--- client/src/short/commentBar.js
+++ client/src/short/commentBar.js
@@ -5,17 +5,12 @@ class CommentBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      comments: [
-        // "SMOFGKNSDMGLK SDMGLK DSMGLKDS GMKLS DGMLKSD GMLKSD GMLDSK GSMOFGKNSDMGLK SDMGLK DSMGLKDS GMKLS DGMLKSD GMLKSD GMLDSK GSMOFGKNSDMGLK SDMGLK DSMGLKDS GMKLS DGMLKSD GMLKSD GMLDSK G",
-        // "cock and balls !",
-        // "i love cummies",
-        // "cock and ball TORTURE!!! I FUCKING LOVE THAT STUFF!!!!!!",
-        // "KC le KK xD",
-      ],
+      comments: [],
       userInput: "",
     };
     this.postComment = this.postComment.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.closeComments = this.closeComments.bind(this);
   }
 
   async componentDidMount() {
@@ -69,13 +64,17 @@ class CommentBar extends React.Component {
       this.setState({ userInput: "" });
     }
   }
+  
+  closeComments(){
+    this.props.setState({commentsShown: false})
+  }
 
   render() {
     return (
       <div className={commentsDivStyle}>
         {/* Upper title section */}
-        <div className="flex justify-between items-center">
-          <div className="flex flex-row text-lg p-3 space-x-2">
+        <div className="flex justify-between items-center sticky top-0 bg-white py-[5px] z-10">
+          <div className="flex flex-row text-lg px-3 space-x-2">
             <h1>
               <strong> Comments </strong>
             </h1>
@@ -83,26 +82,26 @@ class CommentBar extends React.Component {
             {/* Comments count */}
           </div>
 
-          <div className="flex flex-row text-lg p-3 space-x-2">
+          <div className="flex flex-row text-lg px-3 space-x-2">
             <button className="h-[5vh] w-[5vh] flex items-center justify-center">
               <img src="commentsFilter.png" className="scale-75" />
             </button>
 
             <button className="h-[5vh] w-[5vh] flex items-center justify-center hover:bg-[#e5e5e5] hover:ease-in-out hover:rounded-full hover:animate-pulse duration-300">
-              <img src="commentsArrow.png" className="scale-75" />
-            </button>
+              <img src="commentsArrow.png" className="scale-75" onClick={this.closeComments}/>
+            </button> 
           </div>
         </div>
 
         {/* Displayed comments section */}
-        <div className="text-[0.9rem] flex flex-column p-[2vh]">
+        <div className="text-[0.9rem] flex flex-column px-[2vh]">
           {/* Single comment renderer*/}
-          <div id="comment" className="items-stretch">
+          <div id="comment">
             {this.state.comments.map((comment) => (
-              <div className="flex flex-row">
-                <div className="rounded-full h-[4.5vh] w-[4.5vh] bg-[#e5e5e5]"></div>
+              <div className="flex flex-row my-2">
+                <div className="rounded-full h-[4.5vh] w-[4.5vh] bg-[#e5e5e5] content-start"></div>
 
-                <div className="p-[1vh] max-w-[85%]">
+                <div className="px-[1.9vh] max-w-[88%]">
                   <div className="text-sm mb-[3px]">
                     <strong> @Name </strong>
                     <span className="text-[#525252] text-xs"> 1 year ago </span>
@@ -112,37 +111,56 @@ class CommentBar extends React.Component {
                     {comment}
                   </div>
 
-                  <div className="flex flex-row items-center">
-                    <button className="h-[3vh] w-[3vh] flex items-center justify-center">
-                      <img src="commentLikeButton.png" className="scale-[90%]" />
+                  <div className="flex flex-row items-center text-xs">
+                    <div className="flex flex-row items-center">
+                      <div className="flex flex-row items-center">
+                        <button className="h-[3vh] w-[3vh] flex items-center justify-center">
+                          <img src="commentLikeButton.png" className="scale-[90%]" />
+                        </button>
+                        <span className="text-[#525252] ml-[3px]"> 3.5K </span>
+                      </div>
+                      <button className="h-[5vh] w-[5vh] flex items-center justify-center">
+                        <img src="commentDislikeButton.png" className="scale-[55%]" />
+                      </button>
+                    </div>
+
+                    <button className="ml-[3px] hover:bg-[#e5e5e5] rounded-[30px] px-[12px] py-[9px]">
+                       <strong> Reply </strong>
                     </button>
-                    <button className="h-[5vh] w-[5vh] flex items-center justify-center">
-                      <img src="commentDislikeButton.png" className="scale-[55%]" />
-                    </button>
+
                   </div>
 
                 </div>
               </div>
             ))}
           </div>
+        </div>
 
-          {/* Comment insert section  
-          <div className='sticky bottom-0'>
-            <input
-              id="commentsInputField"
-              className=""
-              maxLength="1024"
-              type="text"
-              onChange={this.handleChange}
-            />
+        {/* Comment insert section */}
+        <div className='flex-end sticky bottom-0 bg-white min-width-[100%] border-t-[1px]'>
+
+          <div className="flex flex-row no-wrap p-[1.8vh] items-center">
+            <div className="rounded-full h-[4.5vh] w-[4.5vh] bg-[#e5e5e5] content-start"></div>
+
+              <input
+                id="commentsInputField"
+                className="outline-0 text-sm px-[2vh]"
+                maxLength="1024"
+                placeholder="Add a comment..."
+                type="text"
+                onChange={this.handleChange}
+              />
+            
             <button
               onClick={this.postComment}
-              className="m-2 px-[1.5vh] py-[0.95vh] rounded-full bg-blue-300"
+              className=" flex-end m-2 rounded-full border-[1px] text-sm px-[15px] py-[7px] hover:bg-[#e5e5e5] hover:ease-in-out duration-300"
             >
-              Post comment
+              <strong>Post</strong>
             </button>
-          </div>*/}
-        </div>
+
+          </div>
+
+          </div>
       </div>
     );
   }
@@ -151,4 +169,4 @@ class CommentBar extends React.Component {
 export default CommentBar;
 
 const commentsDivStyle =
-  "flex flex-col shadow-2xl translate-y-px ml-[0.95vh] h-[80vh] w-[45vh] break-words rounded-[0.7vh] rounded-r-lg overflow-y-auto";
+  "flex flex-col shadow-2xl translate-y-px h-[80vh] w-[45vh] break-words rounded-[0.7vh] rounded-r-lg overflow-y-auto";
