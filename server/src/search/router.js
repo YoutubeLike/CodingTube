11f99462e12940c7764419ad7cffa0184f85bfa3
TheStarBars <aoges@orange.fr>
2024-03-21 15:43:35 +0100

    add router and sql request for search page

--- server/src/search/router.js
+++ server/src/search/router.js
@@ -7,6 +7,7 @@ const { history } = require('./history.js')
 const { deleteHistory } = require('./deleteHistory.js')
 const { mostResearch_onChange} = require('./mostResearch_onChange.js')
 const { history_onChange } = require('./history_onChange.js')
+const { displaySearchPage } = require('./displaySearchPage.js')
 
 
 router.get('/request/:submit', submit)
@@ -15,6 +16,7 @@ router.get('/history/:history', history)
 router.get('/deleteHistory/:deleteHistory', deleteHistory)
 
 router.get('/mostResearch_onChange/:researchInput', mostResearch_onChange)  
-router.get('/history_onChange/:userId/:researchInput', history_onChange)  
+router.get('/history_onChange/:userId/:researchInput', history_onChange)
+router.get('/displaySearchPage/:researchInput', displaySearchPage)  
 
 module.exports = router
