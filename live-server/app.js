4163c4fe07a95af86b26f438c270a52ab6e182c6
Stefan Dupont <dupontstef80@gmail.com>
2024-03-21 09:43:34 +0100

    feat(live): replay

--- live-server/app.js
+++ live-server/app.js
@@ -14,20 +14,12 @@ const config = {
     allow_origin: '*'
   },
   trans: {
-    ffmpeg: '/usr/local/bin/ffmpeg',
+    ffmpeg: '/usr/bin/ffmpeg',
     tasks: [
       {
         app: 'live',
-        vc: "copy",
-        vcParam: [],
-        ac: "aac",
-        acParam: ['-ab', '64k', '-ac', '1', '-ar', '44100'],
-        rtmp:true,
-        rtmpApp:'live2',
-        hls: true,
-        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-        dash: true,
-        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+        mp4: true,
+        mp4Flags: '[movflags=frag_keyframe+empty_moov]',
       }
     ]
   }
