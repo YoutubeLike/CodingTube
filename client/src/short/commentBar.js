926111b4839d1b17a45a0b5b3e483147c2081bc3
Lina <lina.meniai@edu.esiee-it.fr>
2024-03-21 09:38:03 +0100

    css in comment bar

--- client/src/short/commentBar.js
+++ client/src/short/commentBar.js
@@ -37,36 +37,76 @@ class CommentBar extends React.Component {
 
   render() {
     return (
-      <div className="ml-[0.95vh] h-[80vh] w-[55vh] flex flex-col justify-between break-words bg-gray-300 rounded-[0.7vh] rounded-r-xl">
-        {/* Comments count */}
-        <div className="p-2">
-          Comments <strong>{this.state.comments.length}</strong>
+      <div className={commentsDivStyle}>
+        {/* Upper title section */}
+        <div className="flex justify-between items-center">
+          <div className="flex flex-row text-lg p-3 space-x-2">
+            <h1>
+              <strong> Comments </strong>
+            </h1>
+            <h1 className="text-[#525252]"> {this.state.comments.length} </h1>
+            {/* Comments count */}
+          </div>
+
+          <div className="flex flex-row text-lg p-3 space-x-2">
+            <button className="h-[5vh] w-[5vh] flex items-center justify-center">
+              <img src="commentsFilter.png" className="scale-75" />
+            </button>
+
+            <button className="h-[5vh] w-[5vh] flex items-center justify-center hover:bg-[#e5e5e5] hover:ease-in-out hover:rounded-full hover:animate-pulse duration-300">
+              <img src="commentsArrow.png" className="scale-75" />
+            </button>
+          </div>
         </div>
+
         {/* Displayed comments section */}
-        <div className="overflow-scroll">
-          <div id="comments" className="flex-col-reverse p-[1vh]">
+        <div className="text-[0.9rem] flex flex-column p-[2vh]">
+          {/* Single comment renderer*/}
+          <div id="comment" className="items-stretch">
             {this.state.comments.map((comment) => (
-              <div className="mx-[1%] my-[1vh] rounded-sm bg-white">
-                {comment}
+              <div className="flex flex-row">
+                <div className="rounded-full h-[4.5vh] w-[4.5vh] bg-[#e5e5e5]"></div>
+
+                <div className="p-[1vh] max-w-[85%]">
+                  <div className="text-sm mb-[3px]">
+                    <strong> @Name </strong>
+                    <span className="text-[#525252] text-xs"> 1 year ago </span>
+                  </div>
+
+                  <div className="text-ellipsis overflow-hidden">
+                    {comment}
+                  </div>
+
+                  <div className="flex flex-row items-center">
+                    <button className="h-[3vh] w-[3vh] flex items-center justify-center">
+                      <img src="commentLikeButton.png" className="scale-[90%]" />
+                    </button>
+                    <button className="h-[5vh] w-[5vh] flex items-center justify-center">
+                      <img src="commentDislikeButton.png" className="scale-[55%]" />
+                    </button>
+                  </div>
+
+                </div>
               </div>
             ))}
           </div>
-          {/* Comment insert section  */}
-          <div>
+
+          {/* Comment insert section  
+          <div className='sticky bottom-0'>
             <input
               id="commentsInputField"
-              className="mx-[3%] w-[94%] border border-purple border-50 rounded-sm text-gray"
+              className=""
               maxLength="1024"
               type="text"
               onChange={this.handleChange}
             />
             <button
               onClick={this.postComment}
-              className="m-2 px-[1.5vh] py-[0.95vh] text-[1.5vh] rounded-full bg-blue-300"
+              className="m-2 px-[1.5vh] py-[0.95vh] rounded-full bg-blue-300"
             >
               Post comment
             </button>
-          </div>
+          </div>*/}
         </div>
       </div>
     );
@@ -74,3 +114,6 @@ class CommentBar extends React.Component {
 }
 
 export default CommentBar;
+
+const commentsDivStyle =
+  "flex flex-col shadow-2xl translate-y-px ml-[0.95vh] h-[80vh] w-[45vh] break-words rounded-[0.7vh] rounded-r-lg overflow-y-auto";
