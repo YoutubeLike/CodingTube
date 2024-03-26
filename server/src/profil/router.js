0106f3ca1d4fb5cf1fd6161b060ce3a046808df2
Stefan Dupont <dupontstef80@gmail.com>
2024-03-26 22:19:58 +0100

    fix(profil) : duplicate code

--- server/src/profil/router.js
+++ server/src/profil/router.js
@@ -114,18 +114,6 @@ router.post("/login", async (req, res) => {
     // Check if username or email exists in the database
     const usernameExist = await CheckIfUsernameExist(loginData.usernameOrMail);
     const mailExist = await CheckIfMailExist(loginData.usernameOrMail);
-
-    const passwordFromDb = await GetPasswordFromUsernameOrEmail(loginData.usernameOrMail);
-    const isPasswordMatch = await CheckIfPasswordMatch(loginData.password, passwordFromDb)
-
-    if (loginData.usernameOrMail != "" && loginData.password != "") {
-
-      if (usernameExist || mailExist) {
-        if (isPasswordMatch) {
-          console.log("User signed in successfully");
-          return res.status(200).json({ message: "User signed in successfully" });
-
-
     // Get password associated with username or email from the database
     const passwordFromDb = await GetPasswordFromUsernameOrEmail(
       loginData.usernameOrMail
