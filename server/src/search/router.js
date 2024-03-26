ecda53299249101dd0e694105d858386f870025f
Stefan Dupont <dupontstef80@gmail.com>
2024-03-26 22:38:17 +0100

    fix(git): rewind to 3a522635de68a2a4f28a10be27222c538f87323a

--- server/src/search/router.js
+++ server/src/search/router.js
@@ -19,5 +19,5 @@ router.get('/mostResearch_onChange/:researchInput', mostResearch_onChange)
 router.get('/history_onChange/:userId/:researchInput', history_onChange)
 router.get('/displaySearchPage/:researchInput', displaySearchPage)  
 router.get('/request/:submit', submit)
- 
+
 module.exports = router
