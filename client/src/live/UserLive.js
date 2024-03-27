6f502c7aace201c0e87929b6de84f883c139b9fe
PetitBidule <121752200+PetitBidule@users.noreply.github.com>
2024-03-27 15:51:32 +0100

    feature:update-front

--- client/src/live/UserLive.js
+++ client/src/live/UserLive.js
@@ -10,47 +10,61 @@ export async function loader({ params }) {
   return { user };
 }
 
-export default function LiveFromUser()
-{
-    const {user} = useLoaderData()
-    const [UserSize, setUserSize] = useState()
-    const socket = io("http://localhost:5000")
-    const [Socket, SetSocket] = useState(socket);
-    
-    
-    useEffect(() => {
-        // SetSocket(socket);
-        socket.emit("AskUserCount", {user : user});
+export default function LiveFromUser() {
+  const { user } = useLoaderData();
+  const [UserSize, setUserSize] = useState();
+  const socket = io("http://localhost:5000");
+  const [Socket, SetSocket] = useState(socket);
 
-        socket.on("user-count", (arg) => {
-            
-        setUserSize(arg.size)
-        console.log(arg.size)
-        
-    }, [])  
-    })
+  useEffect(() => {
+    // SetSocket(socket);
+    socket.emit("AskUserCount", { user: user });
 
-    console.log(Socket)
+    socket.on(
+      "user-count",
+      (arg) => {
+        setUserSize(arg.size);
+        console.log(arg.size);
+      },
+      []
+    );
+  });
 
-    return (
-        <>
-            <p> {user} </p>
-            <div className="section-live-chat flex flex-col items-start md:flex-row w-full h-screen">
-            <div style={{width: "640px", height: "360px"}} class="video-container">
-                <ReactPlayer style={{position: "absolute", zIndex: "2"}} className="video-container" url={"http://localhost:8090/live/" + user + ".flv"} controls={false} playing={false} pip={false} />
-            <img style={{position: "absolute", width: "640px"}} src="/live/offline.jpg" />
-            </div>
-            <Chat user={user} socket={Socket} />
-            <p>{UserSize}</p>
-            </div>
-            {/* <button onClick={SendWidget}> Appuye moi </button> */}
-            
-        </>
-    )
+  console.log(Socket);
+
+  return (
+    <>
+      <p> {user} </p>
+      <div className="section-live-chat flex flex-col items-start md:flex-row w-full h-screen">
+        <div
+          //   style={{ width: "640px", height: "360px" }}
+          class="video-container w-auto flex flex-col"
+        >
+          <div>
+            <ReactPlayer
+              style={{ position: "absolute", zIndex: "2" }}
+              className="video-container"
+              url={"http://localhost:8090/live/" + user + ".flv"}
+              controls={false}
+              playing={false}
+              pip={false}
+            />
+            <img
+              style={{ position: "relative", top: "0" }}
+              src="/live/offline.jpg"
+            />
+          </div>
+
+          <p>{UserSize}bonjour les gens </p>
+        </div>
+
+        <Chat user={user} socket={Socket} />
+      </div>
+      {/* <button onClick={SendWidget}> Appuye moi </button> */}
+    </>
+  );
 }
 
-
-export async function SendWidget()
-{
-    io("http://localhost:5000").emit("send")
+export async function SendWidget() {
+  io("http://localhost:5000").emit("send");
 }
