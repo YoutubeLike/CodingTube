c05a4f83beea206edf5f723dcf00d9946e6cb205
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2024-03-29 11:20:26 +0100

    removed unused file

--- client/src/short/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ShortRequest from "./shortRequest";
-import ReactDOM from "react-dom";
-import axios from "axios";
-import Short from "./short.js";
-
-export default Short;
